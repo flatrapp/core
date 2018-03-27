f888613c3d37a892c297e54680914058706a655f
Daniel Schaefer <git@danielschaefer.me>
2018-03-27 22:55:23 +0700

    Simpler nixpkgs pinning
    Now it is not dependent on anything on the system except for
    Nix version > 1.12
    Used this method:
    https://nixos.wiki/wiki/How_to_fetch_Nixpkgs_with_an_empty_NIX_PATH

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,5 +1,10 @@
 let
-  _nixpkgs = import <nixpkgs> {};
-  nixpkgs = _nixpkgs.fetchFromGitHub (_nixpkgs.lib.importJSON ./src.json);
+  fetchNixpkgs = { rev, sha256 } : builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
 in
-  import nixpkgs
+  import (fetchNixpkgs {
+    rev = "5141f28405e5d31f21c10869dfc86ff340053787";
+    sha256 = "0q91kfxg950g1nr71ifxhb4gfn3vfs4szh2yn7z8s2xri4l36p5m";
+  })
